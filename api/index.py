254fd43c07668d461ea2ea5d8355e923b5110feb
tom-dot3 <taylorgarh@outlook.com>
2026-01-14 18:29:16 -0800

    Create index.py

---
+++ api/index.py
@@ -0,0 +1,23 @@
+import json
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/", response_class=HTMLResponse)
+async def index():
+    with open("static/index.html", encoding="utf-8") as f:
+        return f.read()
+
+@app.post("/collect")
+async def collect(request: Request):
+    data = await request.json()
+
+    print("==== NEW VISITOR ====")
+    print(json.dumps(data, indent=2))
+    print("=====================")
+
+    return {"status": "ok"}
